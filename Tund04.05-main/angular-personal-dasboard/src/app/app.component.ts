1d8a6d9c33488b94c32c56c710ac04e4a6792b47
Opilane TTHK <opilane@tthk.ee>
2023-05-11 12:00:11 +0300

    123

---
+++ Tund04.05-main/angular-personal-dasboard/src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { animate, query, style, transition, trigger } from '@angular/animations';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger("routeAnim", [
+      transition("* => *", [
+        
+        query (':leave',[
+          style({
+            display: "block"
+          }),
+          animate(1000, style({
+            opacity: 0
+          }))
+        ], {optional: true}),
+
+        query(":enter",[
+          style({
+            opacity:0,
+            display: "block",
+            height:"100%"
+          }),
+          animate(1000, style({
+            opacity: 1
+          }))
+        ], {optional: true})
+      ])
+    ])
+  ]
+})
+export class AppComponent {
+  
+
+  prepareRoute(outlet: RouterOutlet) {
+    if (outlet.isActivated) {
+      const tab = outlet.activatedRouteData['tab']
+      if (!tab) return 'secondary'
+      return tab
+    }
+  }
+}
